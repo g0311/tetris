2859453473226e8d17b19588cbf189785d1a6aa2
g0311 <gsc0815@naver.com>
2023-11-10 02:40:11 +0900

    231109

--- Assets/Codes/Board.cs
+++ Assets/Codes/Board.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
+    public Transform[,] grid = new Transform[10, 20]; //각 좌표마다 타일을 저장할 배열
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
+
+    int point = 0;
     void Update()
     {
-        
+        for(int i = 0; i < 20; i++)
+        {
+            if (checkLineFull(i))
+            {
+                point++;
+                DestroyLine(i);
+            }
+        }
+    }
+
+    bool checkLineFull(int y)
+    {
+         for(int i = 0; i < 10; i++)
+        {
+            if(grid[i,y] == null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    void DestroyLine(int y)
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            Destroy(grid[i, y].gameObject);
+            grid[i, y] = null;
+        }
     }
 }
---
+++ Assets/Codes/TetroBehav.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetroBehav : MonoBehaviour
+{
+    public GameObject parentBoard;
+    private Transform[,] grid;
+    private bool movable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movable = true;
+        grid = parentBoard.GetComponent<Board>().grid;
+    }
+    float curt = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        if (movable)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                TMove(0);
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                TMove(1);
+            }
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                TRotate();
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                curt = 1;
+            }
+
+            curt += Time.deltaTime;
+            if (curt >= 1)
+            {
+                TFall();
+                curt = 0;
+            }
+        }
+    }
+
+    //충돌 범위 설정 /떨어질때 마다 체크? 회전도?
+    bool isValidMove()
+    {
+        foreach(Transform children in transform)
+        {
+            int x = (int)children.position.x - (int)parentBoard.transform.position.x; //한 타일의 x좌표
+            int y = (int)children.position.y - (int)parentBoard.transform.position.y; //한 타일의 y좌표
+            //근데 이거 얘네 좌표가 절대 좌표 기준 아님? 그러면 좌표알아야함
+            if(x < 0 || x >= 10 || y < 0 || y >= 20)
+            {
+                return false;
+            }
+
+            if(grid[x,y] != null)
+            {
+                return false;
+            }
+        }
+        return true;
+    } //true일시 이동, false일시 이동 되돌리기
+    void TFall() //떨어지게 설정
+    {
+        transform.position += new Vector3(0, -1, 0);
+        if (!isValidMove())
+        {
+            transform.position -= new Vector3(0, -1, 0);
+            save();
+        }
+    }
+    void save()
+    {
+        foreach (Transform children in transform)
+        {
+            int x = (int)children.position.x - (int)parentBoard.transform.position.x; //한 타일의 x좌표
+            int y = (int)children.position.y - (int)parentBoard.transform.position.y; //한 타일의 y좌표
+            grid[x, y] = children;
+        }
+        movable = false;
+    }
+    void TRotate() //회전 구현
+    {
+        transform.Rotate(new Vector3(0, 0, 90));
+        if (!isValidMove())
+        {
+            transform.Rotate(new Vector3(0, 0, -90));
+        }
+    }
+    void TMove(int way) //이동 구현
+    {
+        if (way == 1)
+        {
+            transform.position += new Vector3(1, 0, 0);
+            if (!isValidMove())
+            {
+                transform.position -= new Vector3(1, 0, 0);
+            }
+        }
+        else
+        {
+            transform.position -= new Vector3(1, 0, 0);
+            if (!isValidMove())
+            {
+                transform.position += new Vector3(1, 0, 0);
+            }
+        }
+    }
+}
